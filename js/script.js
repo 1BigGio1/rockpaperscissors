714492ffc56c717954b07071087cdd322bd5f2bf
Jovanni Pelaez <pelaezjovanni3@gmail.com>
2018-11-19 23:12:11 +0000

    changes

--- js/script.js
+++ js/script.js
@@ -4,10 +4,75 @@
 
 //GLOBAL VARIABLES
 /* global $ */
+var user;
+
 $("#shoot").click(function(){
-    $("#result").text($("#input").val());
-    
+    user = $("#input").val();
+    result();
 });
+
+function result(){ 
+  var cpt = Math.random() * 59 + 1;
+  if (cpt < 20.0) {
+    cpt = "rock";
+  } else if(cpt <= 40.0) {
+    cpt = "paper";
+  } else{
+    cpt = "scissors";
+  } 
+  
+  
+  if(user === "rock"){
+    if(cpt === "rock"){
+      $("#result").html("It's a Tie");
+    } else if(user === "rock"){
+      if(cpt === "scissors"){
+        $("#result").html("You Win.")
+          } else if(user === "rock"){
+            if(cpt === "paper"){
+              $("#result").html("CPU Wins!!");
+            }
+          }
+        }
+      }
+
+  
+  
+  if(user === "paper"){
+    if(cpt === "rock"){
+      $("#result").html("You Win.");
+    } else if(user === "paper"){
+      if(cpt === "scissors"){
+        $("#result").html("CPU Wins!!");
+          } else if(user === "paper"){
+            if(cpt === "paper"){
+              $("#result").html("It's a Tie.");
+            }
+          }
+        }
+      }
+  
+  
+  if(user === "scissors"){
+    if(cpt === "paper"){
+      $("#result").html("You Win.");
+    } else if(user === "scissors"){
+      if(cpt === "rock"){
+        $("#result").html("CPU Wins!!");
+          } else if(user === "scissors"){
+            if(cpt === "scissors"){
+              $("#result").html("It's a Tie.");
+            }
+          }
+        }
+      }
+  
+  
+ 
+ 
+  $("#userChoice").html(user);
+  $("#computerChoice").html(cpt); 
+}
 
 
 // DOCUMENT READY FUNCTION BELOW
